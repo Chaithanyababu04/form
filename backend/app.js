47ad101d141718af16bba25d28ad1fac2820386a
Chaitanya Babu Gannamani <chaithanyababugannamani@gmail.com>
2026-01-28 15:32:32 +0530

    First commit'

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./db");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/submit", (req, res) => {
+  const { name, email } = req.body;
+
+  db.query(
+    "INSERT INTO users (name, email) VALUES (?, ?)",
+    [name, email],
+    (err) => {
+      if (err) return res.status(500).send("DB Error");
+      res.send("Data saved successfully!");
+    }
+  );
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
