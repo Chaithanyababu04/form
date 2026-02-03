640386f54400cbd2abe7a559476b89c33ec407e1
Chaitanya Babu Gannamani <chaithanyababugannamani@gmail.com>
2026-02-03 11:51:21 +0530

    app.js

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,6 @@ app.post("/submit", (req, res) => {
   );
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
 });
